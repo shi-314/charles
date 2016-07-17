6d8d9d945ce5525bdaca63d7dcbcf162890bef5a
Shivan <shi.taher@gmail.com>
2016-07-17 23:02:38 +0200

    Sphere hit detection

--- main.cpp
+++ main.cpp
@@ -5,7 +5,19 @@
 
 using namespace std;
 
+bool hit_sphere(const vec3f& center, float radius, const rayf& r) {
+    vec3f oc = r.origin() - center;
+    float a = dot(r.direction(), r.direction());
+    float b = 2.0f * dot(oc, r.direction());
+    float c = dot(oc, oc) - radius * radius;
+    float discriminant = b * b - 4.0f * a * c;
+    return discriminant > 0;
+}
+
 vec3f color(const rayf& r) {
+    if(hit_sphere(vec3f(0, 0, -1), 0.5f, r))
+        return vec3f(1, 0, 0);
+
     vec3f unit_direction = unit_vec(r.direction());
     float t = .5f * (unit_direction.y() + 1.0f);
     return (1.0f - t) * vec3f(1.0f, 1.0f, 1.0f) + t * vec3f(.5f, .7f, 1.0f);
@@ -18,7 +30,7 @@ int main() {
     cout << "P3\n" << nx << " " << ny << "\n255\n" << endl;
 
     vec3f lower_left_corner(-2.0f, -1.0f, -1.0f);
-    vec3f horizontal(-4.0f, 0.0f, 0.0f);
+    vec3f horizontal(4.0f, 0.0f, 0.0f);
     vec3f vertical(0.0f, 2.0f, 0.0f);
     vec3f origin(0.0f, 0.0f, 0.0f);
 
